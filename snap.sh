0abdebef7e57da69303736f3e9133ab4435d38a7
Andrei <andrei1015@gmail.com>
2023-07-24 21:23:14 +0200

    make a function

--- snap.sh
+++ snap.sh
@@ -3,16 +3,19 @@ root='/backups'
 folder=$(date +'%Y-%m-%d-%H-%M-%S')
 current_folder="${root}/${folder}"
 
+snapshot() {
+rsync -aAXHv --mkpath --include-from='include.list' --exclude-from='exclude.list' --exclude="${root}" / "${current_folder}"
+tar cvf "${root}/archives/${folder}.tar" "${current_folder}"
+mkdir -p "${root}/archives"
+rm -rf "${current_folder}"
+}
+
+snapshot
+
+
 # Uncomment these lines for debugging if needed
 # echo "${current_folder}"
 # echo "${root}"
 # echo "${folder}"
 # echo "rsync -aAXHv --mkpath --include-from='include.list' --exclude-from='exclude.list' --exclude='${folder}' / '${current_folder}'"
 # echo "tar cvf '${root}/archives/${folder}.tar' '${current_folder}'" 
-
-mkdir -p "${root}/archives"
-
-rsync -aAXHv --mkpath --include-from='include.list' --exclude-from='exclude.list' --exclude="${root}" / "${current_folder}"
-tar cvf "${root}/archives/${folder}.tar" "${current_folder}"
-
-rm -rf "${current_folder}"
