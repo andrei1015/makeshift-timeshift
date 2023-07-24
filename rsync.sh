84d0a5688e26f7f421b14b9504446ff2bfb84af8
Andrei <andrei1015@gmail.com>
2023-07-24 09:50:59 +0200

    tar also works now

--- rsync.sh
+++ rsync.sh
@@ -7,9 +7,12 @@ current_folder="${root}/${folder}"
 # echo "${current_folder}"
 # echo "${root}"
 # echo "${folder}"
+# echo "rsync -aAXHv --mkpath --include-from='include.list' --exclude-from='exclude.list' --exclude='${folder}' / '${current_folder}'"
+# echo "tar cvf '${root}/archives/${folder}.tar' '${current_folder}'" 
+
+mkdir -p "${root}/archives"
 
 rsync -aAXHv --mkpath --include-from='include.list' --exclude-from='exclude.list' --exclude="${root}" / "${current_folder}"
 tar cvf "${root}/archives/${folder}.tar" "${current_folder}"
 
-# echo "rsync -aAXHv --mkpath --include-from='include.list' --exclude-from='exclude.list' --exclude='${folder}' / '${current_folder}'"
-# echo "tar cvf '${root}/archives/${folder}.tar' '${current_folder}'" 
+rm -rf "${current_folder}"
